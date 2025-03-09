f59c6a3235d88cb5a88945c05221a9cfecf4bb38
israel <israel_lima_16@hotmail.com>
2025-03-09 11:40:12 -0300

    feat: criar massa sql

--- src/main/java/com/lima/api/cine/CinePipocaApplication.java
+++ src/main/java/com/lima/api/cine/CinePipocaApplication.java
@@ -1,64 +1,16 @@
 package com.lima.api.cine;
 
-import com.lima.api.cine.enun.FormaPagamento;
-import com.lima.api.cine.enun.IdiomaFilme;
-import com.lima.api.cine.model.Cliente;
-import com.lima.api.cine.model.Filme;
-import com.lima.api.cine.model.Sala;
-import com.lima.api.cine.model.Sessao;
-import com.lima.api.cine.repository.ClienteRepository;
-import com.lima.api.cine.repository.FilmeRepository;
-import com.lima.api.cine.repository.SessaoRepository;
-import com.lima.api.cine.service.IngressoService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 
 // TODO: Aplicar um formatador/lint exemplo checkstyle ou spotless
 // TODO: O projeto utiliza bastante manutenção de estados. Check a possibilidade de aplicar o pattern GOF state
 
 @SpringBootApplication
-public class CinePipocaApplication implements CommandLineRunner {
-
-	@Autowired
-	FilmeRepository filmeRepository;
-
-	@Autowired
-	SessaoRepository sessaoRepository;
-
-	@Autowired
-	ClienteRepository clienteRepository;
-
-	@Autowired
-	IngressoService ingressoService;
+public class CinePipocaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CinePipocaApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-
-		Filme filme = new Filme("O auto da compadecida 2", IdiomaFilme.DUBLADO, "2h");
-		Sala sala = new Sala("A");
-		LocalDateTime agora = LocalDateTime.now();
-		LocalDateTime dataHoraInicio = agora.withHour(16).withMinute(0).withSecond(0).withNano(0);
-		LocalDateTime dataHoraFim = dataHoraInicio.plus(2, ChronoUnit.HOURS);
-
-		//filmeRepository.save(filme);
-		//filme = filmeRepository.findById(1L).get();
-
-		//Sessao sessao = new Sessao(dataHoraInicio, dataHoraFim, filme, sala, new BigDecimal("40.00"));
-		//sessaoRepository.save(sessao);
-
-		Cliente cliente = new Cliente("João");
-		clienteRepository.save(cliente);
-
-		Sessao sessao = sessaoRepository.findById(1L).get();
-		ingressoService.comprar(cliente, sessao, 4, FormaPagamento.PIX);
-	}
 }
---
+++ src/main/resources/script/massa.sql
@@ -0,0 +1,41 @@
+-- Filtros
+-- select * from db_cine_pipoca.tbl_filme tf;
+-- select * from db_cine_pipoca.tbl_sala ts;
+-- select * from db_cine_pipoca.tbl_assento ta;
+-- select * from db_cine_pipoca.tbl_sessao ts;
+-- select * from db_cine_pipoca.tbl_pagamento tp;
+-- select * from db_cine_pipoca.tbl_cliente tc;
+-- select * from db_cine_pipoca.tbl_ingresso ti;
+
+-- Criar cliente
+insert into db_cine_pipoca.tbl_cliente (nome) values ("Ryan");
+
+-- Criar filme
+insert into  db_cine_pipoca.tbl_filme (titulo, idioma, duracao) values ("O auto da compadecida", "DUBLADO", "2h");
+insert into  db_cine_pipoca.tbl_filme (titulo, idioma, duracao) values ("A garota da vez", "LEGENDADO", "1h30m");
+insert into  db_cine_pipoca.tbl_filme (titulo, idioma, duracao) values ("A procura da felicidade", "LEGENDADO", "2h10m");
+
+-- Criar sala
+insert into db_cine_pipoca.tbl_sala (nome, status) values ("A", "FECHADA");
+
+-- Criar assento
+insert into db_cine_pipoca.tbl_assento (numero, status, cliente_id, sala_id) values (1, "VAZIO", null, 1);
+insert into db_cine_pipoca.tbl_assento (numero, status, cliente_id, sala_id) values (2, "VAZIO", null, 1);
+insert into db_cine_pipoca.tbl_assento (numero, status, cliente_id, sala_id) values (3, "VAZIO", null, 1);
+insert into db_cine_pipoca.tbl_assento (numero, status, cliente_id, sala_id) values (4, "VAZIO", null, 1);
+insert into db_cine_pipoca.tbl_assento (numero, status, cliente_id, sala_id) values (5, "VAZIO", null, 1);
+insert into db_cine_pipoca.tbl_assento (numero, status, cliente_id, sala_id) values (6, "VAZIO", null, 1);
+insert into db_cine_pipoca.tbl_assento (numero, status, cliente_id, sala_id) values (7, "VAZIO", null, 1);
+insert into db_cine_pipoca.tbl_assento (numero, status, cliente_id, sala_id) values (8, "VAZIO", null, 1);
+insert into db_cine_pipoca.tbl_assento (numero, status, cliente_id, sala_id) values (9, "VAZIO", null, 1);
+insert into db_cine_pipoca.tbl_assento (numero, status, cliente_id, sala_id) values (10, "VAZIO", null, 1);
+
+-- Criar sessão
+insert into db_cine_pipoca.tbl_sessao (status, data_hora_inicio, data_hora_fim, valor, filme_id, sala_sessao_id) 
+values ("DISPONIVEL", '2025-03-10 14:30:00', '2025-03-10 16:30:00', '40.00', 1, 1);
+
+-- Criar pagamento
+insert into db_cine_pipoca.tbl_pagamento (forma_pagamento) values ("PIX");
+
+-- Comprar ingresso
+insert into db_cine_pipoca.tbl_ingresso (meia_entrada, valor_total, pagamento_id, cliente_id_ingresso, sessao_id) values (0, '40.00', 1, 1, 1);
